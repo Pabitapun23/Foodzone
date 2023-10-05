26bd8eace5f945b93b61d251ce6444c4e337e363
mattyu <www.yukawai@gmail.com>
2023-10-05 14:13:42 -0400

    create basic login, register and dashboard page

--- drivers-website/server.js
+++ drivers-website/server.js
@@ -1,6 +1,16 @@
 const express = require("express")
 const app = express()
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+app.use(session({
+    secret: 'secretOfFullStackDev',
+    resave: false,
+    saveUninitialized: true,
+}))
+app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 const HTTP_PORT = process.env.PORT || 3000
 const mongoose = require("mongoose")
 
@@ -10,10 +20,90 @@ mongoose.connect(CONNECTION_STRING);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Error connecting to database: "));
 db.once("open", () => { console.log("Mongo DB connected successfully.");});
-app.get("/", (req, res) => {
-    return res.send("Hello World")
+
+const driverSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    vehicle: String,
+    color: String,
+    license: String,
+})
+const driverAccount = mongoose.model("drivers", driverSchema)
+
+const sessionQuery = (req, res, next) => {
+    console.log(req.session)
+    if (req.session.username) {
+        return next()
+    }
+    return res.redirect("/login")
+}
+
+app.get("/",sessionQuery,(req, res) => {
+    return res.redirect("/dashboard")
 })
 
+app.get("/login", (req, res) => {
+    return res.render("login.hbs",{layout: false})
+})
+
+app.post("/login", async(req, res) => {
+   try{ 
+    const driverFromDB = await driverAccount.findOne({username: req.body.username})
+    if(!driverFromDB){
+        return res.render("login.hbs",{layout: false, error: "User does not exist"})
+    }
+    if(driverFromDB.password !== req.body.password){
+        return res.render("login.hbs",{layout: false, error: "Password is incorrect"})
+    }
+    console.log(driverFromDB)
+    //set session
+    req.session.username = req.body.username;
+    //
+    return res.redirect("/dashboard")
+    }
+    catch(error){
+        console.log(error)
+    }
+})
+
+app.get("/register",(req,res)=>{
+    return res.render("register.hbs",{layout: false})
+})
+
+app.post("/register",async (req,res)=>{
+    try{
+        const driver = await driverAccount.findOne({username: req.body.username})
+        if(driver){
+            return res.render("register.hbs",{layout: false, error: "Username already exists"})
+        }
+        const newDriver = new driverAccount({
+            username: req.body.username,
+            password: req.body.password,
+            name: req.body.fullname,
+            vehicle: req.body.model,
+            color: req.body.color,
+            license: req.body.license,
+        })
+        const result = await newDriver.save()
+
+        // set session
+        req.session.username = req.body.username;
+        //
+        console.log(result)
+        return res.redirect("/Dashboard");
+    } catch(error){
+        console.log(error)
+    }
+})
+
+app.get("/logout", (req, res) => {
+    req.session.destroy()
+    return res.redirect("/login")
+})
+app.get("/Dashboard",sessionQuery,(req, res) => {
+    return res.render("dashboard.hbs",{layout: "main.hbs"})
+})
 app.listen(HTTP_PORT, () => {
     console.log("Server is running on port 3000")
 })
