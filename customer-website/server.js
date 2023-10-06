3b2b277dc106c6d264e86d7e6b311bdafdb40f18
Pabitapun23 <pabita.pun23@gmail.com>
2023-10-06 00:35:28 -0400

    Navbar and Footer added, and Basic UI for order status page of customer-website

--- customer-website/server.js
+++ customer-website/server.js
@@ -3,7 +3,7 @@ const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
 // configure a folder for external css stylesheets and images
-app.use(express.static("assets"));
+app.use(express.static("public"));
 
 // import handlebars
 const exphbs = require("express-handlebars");
@@ -17,7 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 // DATABASE : Connecting to database and setting up your schemas/models (tables)
 /// --------------
 
-// TODO: Replace this connection string with yours
 const mongoose = require("mongoose");
 
 const CONNECTION_STRING =
@@ -31,13 +30,30 @@ db.once("open", () => {
   console.log("Mongo DB connected successfully.");
 });
 
+// Endpoint for homepage
+app.get("/", (req, res) => {
+  res.render("homepage-template", {
+    layout: "my-layouts",
+  });
+});
 
+// Endpoint for order form page
+app.get("/order-form", (req, res) => {
+  res.render("order-form-page", {
+    layout: "my-layouts",
+  });
+});
 
-
-
+// Endpoint for order status page
+app.get("/order-status", (req, res) => {
+  res.render("order-status-page", {
+    layout: "my-layouts",
+  });
+});
 
 // ----------------
 const onHttpStart = () => {
+  console.log("The web server has started...");
   console.log(`Express web server running on port: ${HTTP_PORT}`);
   console.log(`Press CTRL+C to exit`);
 };
